ac44fb964e5c48bc04d9701d515d30ef92d55f8c
ZhaoChenCheng <zhaochencheng@kashuo.com>
2016-11-13 15:45:41 +0800

    展示优化

--- js/controllers.js
+++ js/controllers.js
@@ -16,8 +16,9 @@ angular.module('PersonalIncomeTax', []).controller('PersonalIncomeTaxController'
 	         	geren:0,
 	         	danwei:0,
 				zongji:0,
-	         	history:[],
 	         	gongjijin:0,
+				 desc:"您不用缴纳个税",
+				 level:0,
 				 isShow:false,
 				 changejishu:function(){
 					 this.jishu=this.shuiqian;
@@ -39,26 +40,32 @@ angular.module('PersonalIncomeTax', []).controller('PersonalIncomeTaxController'
 	         		var yingnashui = this.shuiqian - this.gongjijin - 3500;
 					if(yingnashui<0){
 						this.geshui=0;
-					}
-	         		if(0 < yingnashui && yingnashui <= 1500){
+					}else if(0 < yingnashui && yingnashui <= 1500){
+						this.level=1;
 	         			this.geshui = yingnashui * 3 / 100;
 	         		}
 	         		else if(1500 < yingnashui && yingnashui <= 4500){
+						this.level=2;
 	         			this.geshui = yingnashui * 10 /100 -105;
 	         		}
 	         		else if(4500 < yingnashui && yingnashui <= 9000){
+						this.level=3;
 	         			this.geshui = yingnashui * 20 / 100 - 555;
 	         		}
 	         		else if (9000 < yingnashui && yingnashui <= 35000){
+						this.level=4;
 	         			this.geshui = yingnashui * 25 / 100 - 1005;
 	         		}
 	         		else if(35000 < yingnashui && yingnashui <= 55000){
+						this.level=5;
 	         			this.geshui = yingnashui * 30 / 100 - 2755;
 	         		}
 	         		else if(55000 < yingnashui && yingnashui <= 80000){
+						this.level=6;
 	         			this.geshui = yingnashui * 35 / 100 - 5505;
 	         		}
 	         		else if(yingnashui > 80000){
+						this.level=7;
 	         			this.geshui = yingnashui * 45 / 100 - 13505;
 	         		}
 	         		//计算税后
@@ -74,9 +81,36 @@ angular.module('PersonalIncomeTax', []).controller('PersonalIncomeTaxController'
 					//var part2 = document.getElementById("part2");
 					//part2.top=0;
 					// the element you wish to scroll to.
+					if (this.level>0){
+						this.desc="您属于第"+this.level+"等级，计算公式："
+						switch(this.level)
+						{
+							case 1:
+								this.desc=this.desc+"税前工资-五险一金-（税前工资-五险一金）*3%；具体计算"+this.shuiqian+"-"+this.gongjijin+"-("+this.shuiqian+"-"+this.gongjijin+")"+"*3%="+this.shuihou;
+								break;
+							case 2:
+								this.desc+=this.desc+"税前工资-五险一金-（税前工资-五险一金）*10%+105；具体计算："+this.shuiqian+"-"+this.gongjijin+"-("+this.shuiqian+"-"+this.gongjijin+")"+"*10%+105="+this.shuihou;
+								break;
+							case 3:
+								this.desc+=this.desc+"税前工资-五险一金-（税前工资-五险一金）*20%+555；具体计算："+this.shuiqian+"-"+this.gongjijin+"-("+this.shuiqian+"-"+this.gongjijin+")"+"*20%+555="+this.shuihou;
+								break;
+							case 4:
+								this.desc+=this.desc+"税前工资-五险一金-（税前工资-五险一金）*25%+1005；具体计算："+this.shuiqian+"-"+this.gongjijin+"-("+this.shuiqian+"-"+this.gongjijin+")"+"*25%+1005="+this.shuihou;
+								break;
+							case 5:
+								this.desc+=this.desc+"税前工资-五险一金-（税前工资-五险一金）*30%+2755；具体计算："+this.shuiqian+"-"+this.gongjijin+"-("+this.shuiqian+"-"+this.gongjijin+")"+"*30%+2755="+this.shuihou;
+								break;
+							case 6:
+								this.desc+=this.desc+"税前工资-五险一金-（税前工资-五险一金）*35%+5055；具体计算："+this.shuiqian+"-"+this.gongjijin+"-("+this.shuiqian+"-"+this.gongjijin+")"+"*35%+5055="+this.shuihou;
+								break;
+							case 7:
+								this.desc+=this.desc+"税前工资-五险一金-（税前工资-五险一金）*45%+13505；具体计算："+this.shuiqian+"-"+this.gongjijin+"-("+this.shuiqian+"-"+this.gongjijin+")"+"*45%+13505="+this.shuihou;
+								break;
+						}
+					}
+
 					if ($location.hash() !== 'part3') {
 						$location.hash('part3');
-						$anchorScroll();
 					}else{
 						// call $anchorScroll() explicitly,
 						// since $location.hash hasn't changed
